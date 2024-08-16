6c5b1c3b8193b478ef37ac6b37d25efc0a2d980f
JeanNesi <jeannesi.carlos@gmail.com>
2024-08-16 09:55:50 -0300

    feat: Update authentication callbacks in auth service

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -12,22 +12,6 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     newUser: "/home",
   },
   adapter: PrismaAdapter(prisma),
-  callbacks: {
-    jwt: async (token, user, account) => {
-      if (account) {
-        token.accessToken = account.accessToken;
-        token.refreshToken = account.refreshToken;
-      }
-
-      return Promise.resolve(token);
-    },
-
-    session: async (session, token) => {
-      session.accessToken = token.accessToken;
-      session.refreshToken = token.refreshToken;
-      return Promise.resolve(session);
-    },
-  },
   secret: process.env.AUTH_SECRET,
   providers: [
     EmailProvider({
